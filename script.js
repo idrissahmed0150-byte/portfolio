5ffdfd9b417b0f91759bd23ecdb407a9655e5aa2
idriss <idriss.ahmed0150@gmail.com>
2026-06-06 16:16:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('open');
+});
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('open');
+    });
+});
